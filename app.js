d7b9ec049dbffd7a181cc0bada0a4f29263ca693
Yuki <thankyouforstudying7@gmail.com>
2023-04-17 14:48:36 +0900

    階乗関数実装

--- app.js
+++ app.js
@@ -7,7 +7,10 @@
  */
 function factorial(n) {
   let result = 1;
-  // TODO このコメントを消して正しく実装してください。
+  while (n > 0){
+    result *= n;
+    n--;
+  }
   return result;
 }
 const assert = require('assert');
